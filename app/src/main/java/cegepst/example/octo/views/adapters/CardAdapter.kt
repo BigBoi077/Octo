5e715f0c5c0ea06695aa5f33aa28f1f4bb1bdb34
BigBoi077 <58580199+BigBoi077@users.noreply.github.com>
2021-04-30 09:22:01 -0400

    made adapter and row for card feed

--- app/src/main/java/cegepst/example/octo/views/adapters/CardAdapter.kt
+++ app/src/main/java/cegepst/example/octo/views/adapters/CardAdapter.kt
@@ -3,14 +3,31 @@ package cegepst.example.octo.views.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import cegepst.example.octo.R
 import cegepst.example.octo.models.base.Card
+import com.bumptech.glide.Glide
 
 class CardAdapter(private val cards: List<Card>) : RecyclerView.Adapter<CardAdapter.ViewHolder>() {
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+        private val image = itemView.findViewById<ImageView>(R.id.cardImage)
+        private val name = itemView.findViewById<TextView>(R.id.cardName)
+        private val artist = itemView.findViewById<TextView>(R.id.cardArtist)
+        private val set = itemView.findViewById<TextView>(R.id.cardSet)
+        private val price = itemView.findViewById<TextView>(R.id.cardPrice)
+        private val actionSingle = itemView.findViewById<ImageView>(R.id.actionSingleCard)
+
+        fun setContent(card: Card) {
+            Glide.with(itemView).load(card.imageUris["normal"]).centerCrop().into(image)
+            name.text = card.name
+            artist.text = card.artist
+            set.text = card.setName
+            price.text = card.prices["usd"]
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -20,10 +37,10 @@ class CardAdapter(private val cards: List<Card>) : RecyclerView.Adapter<CardAdap
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.setContent(cards[position])
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return cards.size
     }
 }
